4881fd2739f543497f164e12afad3f491e526c8a
Phan Quân <qmp23@drexel.edu>
2020-05-18 23:27:25 -0400

    Finish setup manager() in trader_operator

--- portfolio_manager.py
+++ portfolio_manager.py
@@ -1,4 +1,4 @@
-from pypfopt.efficient_frontier import EfficientFrontier
+from pypfopt import EfficientFrontier
 from pypfopt import risk_models
 from pypfopt import expected_returns
 
@@ -8,14 +8,16 @@ import pandas as pd
 from pandas_datareader import data as web
 import numpy as np
 from datetime import datetime
+from tqdm import tqdm, tqdm_pandas
 import matplotlib.pyplot as plt
 plt.style.use('fivethirtyeight')
+import os, sys
  
 class PortfolioManager():
 
     def __init__(self, 
-            totalFunding=300, 
-            start_date='2013-01-01', 
+            totalFunding=20000, 
+            start_date="2013-01-01", 
             symbolList=["FB", "AMZN", "AAPL", "NFLX", "GOOG"]):
         
         super().__init__()
@@ -27,16 +29,33 @@ class PortfolioManager():
         self.stockStartDate = start_date
         self.cleaned_weights = None
 
+        # if os.path.exists("./saved_historical_prices.csv"):
+            
+        #     self.df = pd.read_csv("./saved_historical_prices.csv")
+        #     today = datetime.today().strftime('%Y-%m-%d')
+
+        #     print(self.df.loc[["Date", -1]])
+        #     if "Date" in self.df.columns.values and self.df.loc[["Date", -1]] != today:
+                
+        #         for stock in self.assets:
+        #             self.df[stock] = web.DataReader(
+        #                 stock,
+        #                 data_source='yahoo',
+        #                 start=self.df.loc[["Date", -1]], 
+        #                 end=today)['Adj Close']        
+
+
     def setStockPrices(self):
         # Get the stocks ending date aka todays date and format it in the form YYYY-MM-DD
         today = datetime.today().strftime('%Y-%m-%d')
+        tqdm.pandas()
 
         #Store the adjusted close price of stock into the data frame
-        for stock in assets:
+        for stock in self.assets:
             self.df[stock] = web.DataReader(
                 stock,
                 data_source='yahoo',
-                start=stockStartDate , 
+                start=self.stockStartDate , 
                 end=today)['Adj Close']
 
     def plotStockPrices(self):
@@ -59,16 +78,16 @@ class PortfolioManager():
         plt.show()
 
     def calculateMu(self):
-        mu = expected_returns.mean_historical_return(df)#returns.mean() * 252
+        mu = expected_returns.mean_historical_return(self.df)#returns.mean() * 252
         return mu
 
     def calculateSampleCovarianceMatrix(self):
-        S = risk_models.sample_cov(df) #Get the sample covariance matrix
+        S = risk_models.sample_cov(self.df) #Get the sample covariance matrix
         return S
 
-    def optimizePortfolio(self):
+    def optimizePortfolio(self, option='sharpe'):
         '''
-            Optimize for maximal Sharpe ratio
+            Optimize for maximal Sharpe ratio / minimum volatility (Default to Sharpe)
             TODO: Allow for more options
             Return EfficientFrontier object
         '''
@@ -77,8 +96,16 @@ class PortfolioManager():
         S = self.calculateSampleCovarianceMatrix()
         ef = EfficientFrontier(mu, S)
 
-        weights = ef.max_sharpe() #Maximize the Sharpe ratio, and get the raw weights
-        self.cleaned_weights = ef.clean_weights() 
+        if option == 'sharpe':
+            weights = ef.max_sharpe() #Maximize the Sharpe ratio, and get the raw weights
+
+        elif option == 'volatility':
+            weights = ef.min_volatility()
+            
+        else:
+            raise SystemExit("Not found optimize option (Sharpe/Volatility)")
+        
+        self.cleaned_weights = ef.clean_weights()
         print(self.cleaned_weights) #Note the weights may have some rounding error, meaning they may not add up exactly to 1 but should be close
         ef.portfolio_performance(verbose=True)
 
@@ -94,7 +121,7 @@ class PortfolioManager():
 
         latest_prices = get_latest_prices(self.df)
         da = DiscreteAllocation(
-            self.weights, 
+            self.cleaned_weights, 
             latest_prices, 
             total_portfolio_value=self.totalFunding)
 
@@ -103,3 +130,11 @@ class PortfolioManager():
         print("Funds remaining: ${:.2f}".format(leftover))
 
         return allocation, leftover
+
+if __name__ == "__main__":
+
+    pm = PortfolioManager()
+    pm.setStockPrices()
+    pm.plotStockPrices()
+    pm.optimizePortfolio("volatility")
+    pm.allocateFunding()
--- trade_operator.py
+++ trade_operator.py
@@ -1,15 +1,34 @@
 from portfolio_manager import PortfolioManager
 
 import alpaca_trade_api as tradeapi
-from datetime import datetime
-import matplotlib.pyplot as plt
+import os, json
 
 class PortfolioTrader():
 
-    def __init__(self, funding = 500, barTimeInterval = "minute", symbolsList = None):
-        self.api = tradeapi.REST()
+    def __init__(self, 
+        funding=4000, 
+        barTimeInterval = "minute",
+        strategy="sharpe", 
+        symbolsList = None):
+
+        try:
+            with open('./secrets.json') as secrets:
+                data = json.load(secrets)
+
+            self.api = tradeapi.REST(
+                key_id=data['KeyID'],
+                secret_key=data['SecretKey']
+            )
+        except RuntimeError as e:
+            raise SystemExit("Not found secrets API key")
+        
+        self.strategy = strategy
         self.symbols_list = symbolsList
         self.manager = PortfolioManager(totalFunding=funding, symbolList=symbolsList)
+
+        self.manager.setStockPrices()
+        self.manager.optimizePortfolio(strategy)
+        self.manager.allocateFunding()
 
     def place_buy_order(self):
         pass
@@ -19,3 +38,25 @@ class PortfolioTrader():
 
     def system_loop(self):
         pass
+
+
+if __name__ == "__main__":
+    trader = PortfolioTrader(
+        funding=4000,
+        strategy='volatility',
+        symbolsList=[
+            "MDB",
+            "PYPL",
+            "DDOG",
+            "NVDA",
+            "V",
+            "MA",
+            "TEAM",
+            "FB", 
+            "AMZN", 
+            "AAPL", 
+            "NFLX", 
+            "GOOG",
+        ])    
+
+    
